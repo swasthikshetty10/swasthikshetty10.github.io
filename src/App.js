b54595584258c703a38595cfd3fc670176de68ee
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-05-14 00:06:31 +0530

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import react from 'react'
-
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 function App() {
   return (
     <>
-      
+      <Navbar/>
+      <Home/>
     </>
   );
 }
---
+++ src/Components/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Dev from '../../Images/Background/Developer.svg'
+function index() {
+    return (
+        <>
+            <div className = "flex">
+                <img className = "h-56  " src = {Dev}/>
+        </div>
+        </>
+    )
+}
+
+export default index
