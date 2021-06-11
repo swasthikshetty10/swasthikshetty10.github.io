79b2968411455711c70d575f3981aa5d937a9792
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-11 23:44:09 +0530

    Message Form and API call

--- src/Components/Contact/Form.js
+++ src/Components/Contact/Form.js
@@ -1,37 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Slide from 'react-reveal/Slide';
 import axios from 'axios';
 import url from '../../config'
+import Popup from 'react-popup'
+import DataUsageIcon from '@material-ui/icons/DataUsage';
+import DoneIcon from '@material-ui/icons/Done';
 function Form() {
+    const SuccessDiv = React.useRef();
+
+    const [isLoading, setLoading] = useState(false);
     const FormSubmitHandler = (e) => {
         e.preventDefault();
+        setLoading(true)
         const data = {
             name: e.target[0].value,
             email: e.target[1].value,
             phone_no: e.target[2].value,
             message: e.target[3].value
         }
+
         axios.post(url, data)
             .then(res => {
                 console.log(res);
                 console.log(res.data);
+                setLoading(false)
+                SuccessDiv.current.innerHTML = `<p class="font-semibold text-green-600">Thankyou your message was sent ✔️ </p>`
+                e.target[0].value = ""
+                e.target[1].value = ""
+                e.target[2].value = ""
+                e.target[3].value = ""
             })
+            .catch(err => {
+                setLoading(false);
+                SuccessDiv.current.innerHTML = `<p class="font-semibold text-red-600">Sorry something went wrong Try again</p>`
+            })
+
         console.log(data)
-        e.target[0].value = ""
-        e.target[1].value = ""
-        e.target[2].value = ""
-        e.target[3].value = ""
+
 
     }
     return (
         <div className="font-sans flex-grow  m-5 p-5 md:p-10 lg:p-10  border-2  border-blue-700   lg:pb-20 lg:pt-10 lg:pl-10 lg:pr-10  bg-gray-100 rounded-xl " style={{ maxWidth: "600px" }}>
             <div className="flex justify-center"><h2 className=" text-2xl pb-5 md:pb-10 lg:pb-10 pt-5"> Send me a  message 🙂</h2></div>
 
-            <form onSubmit={FormSubmitHandler}>
+            <form onSubmit={FormSubmitHandler} >
                 <div className="name flex flex-col md:flex lg:flex  ">
                     <div className="flex flex-col mr-5">
                         <label>Name</label>
-                        <input className="outline-none border-2 border-opacity-60 border-blue-500  rounded-md p-1 lg:w-8/12 " type="text" required />
+                        <input className="outline-none border-2 border-opacity-60 border-blue-500  rounded-md p-1 lg:w-8/12 " type="text" required disabled={isLoading} />
                     </div>
 
                 </div>
@@ -40,13 +56,13 @@ function Form() {
                     <div className="flex flex-col mt-5">
                         <label className="block">
                             <span className="">Email</span>
-                            <input className="outline-none  border-2 border-opacity-60 border-blue-500 2 rounded-md p-2 w-full form-input mt-1 block w-full" type="email" required />
+                            <input className="outline-none  border-2 border-opacity-60 border-blue-500 2 rounded-md p-2 w-full form-input mt-1 block w-full" type="email" required disabled={isLoading} />
                         </label>
                     </div>
                     <div className="flex flex-col mt-5">
                         <label className="block">
                             <span className="">Contact Number</span>
-                            <input className="outline-none border-2 border-opacity-60 border-blue-500 2 rounded-md p-2 w-full form-input mt-1 block w-full" type="text" />
+                            <input className="outline-none border-2 border-opacity-60 border-blue-500 2 rounded-md p-2 w-full form-input mt-1 block w-full" type="text" disabled={isLoading} />
                         </label>
                     </div>
 
@@ -54,12 +70,15 @@ function Form() {
                 <div className="flex flex-col mt-5">
                     <label className="block">
                         <span classNmae="text-gray-700">Message</span>
-                        <textarea className="outline-none border-2 border-opacity-60  form-textarea mt-1 block w-full border-blue-600 rounded-md p-1 " rows="4" placeholder="message . . ."></textarea>
+                        <textarea className="outline-none border-2 border-opacity-60  form-textarea mt-1 block w-full border-blue-600 rounded-md p-1 " rows="4" placeholder="message . . ." disabled={isLoading}  ></textarea>
 
                     </label>
-                    <button style={{ border: "none" }} type="submit" class="mt-5 border-none bg-blue-500 w-30 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                        Submit
+                    <button type="submit" className="focus:outline-none  mt-5  bg-blue-500 w-30 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" disabled={isLoading}>
+                        {isLoading ? <> <DataUsageIcon className="animate-spin" /> sending</> : 'Submit'}
                     </button>
+                    <div className="text-center mt-3" ref={SuccessDiv}>
+
+                    </div>
                 </div>
             </form>
         </div>
--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-const url = 'http://127.0.0.1:8000/api/contact/'
+const url = "https://api-swasthikshetty10.herokuapp.com/api/contact/"
+const localurl = 'http://127.0.0.1:8000/api/contact/'
+
 export default url
