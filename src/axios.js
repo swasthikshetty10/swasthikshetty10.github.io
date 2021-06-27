697bc560f0e57a5193ca508c99e898ea22987eea
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-06-27 22:47:51 +0530

    axiosInstancer & LoginHandler

---
+++ src/axios.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { urls } from './config'
+const baseURL = urls.api;
+
+const axiosInstance = axios.create({
+    baseURL: baseURL,
+    timeout: 20000,
+    headers: {
+        Authorization: localStorage.getItem('access_token')
+            ? 'JWT ' + localStorage.getItem('access_token')
+            : null,
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+    },
+});
